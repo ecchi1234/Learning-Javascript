3f89a372402a2f394ba386e8a33cb792092904dc
ecchi1234 <62091560+ecchi1234@users.noreply.github.com>
2020-07-31 17:44:29 +0700

    Update WriteNumberinExpandedForm.js

--- WriteNumberinExpandedForm.js
+++ WriteNumberinExpandedForm.js
@@ -1,4 +1,14 @@
 /* Đề bài:
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+
+NOTE: All numbers will be whole numbers greater than 0.
+  
+}
+*/
 function expandedForm(num) {
   var digits = num.toString().split('').reverse().map(function(e, index){
     return (parseInt(e)*Math.pow(10, index)).toString();
@@ -9,6 +19,3 @@ function expandedForm(num) {
   console.log(digits);
   
   return digits;
-  
-}
-*/
